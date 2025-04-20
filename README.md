72fd48c61541275c336a57a4afd5528b1a9c6ca7
Dhruv Chauhan <chauhan.dhr@northeastern.edu>
2025-04-20 15:15:29 -0400

    changed readme a bit more

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# fantasydraftpredictor
+# LLM-powered fantasy drafter
+
+## Relevant Folders & Files
+- `src/draft-simulator/llm_draft_auto_drafting.py` - contains the draft simulator and can be run to simulate a draft.
+- `src/draft-simulator/drafter_multi_agent.py`- contains the logic and architecture for the llm drafter
+- `src/draft-simulator/constrained_logs` - cleaned up version of logs which llm drafter include group chat conversations, selections, and final team 
+- `src/draft-simulator/prompts` - prompts for all agents involved including head drafter, position agents (both extractors and analyzers)
+- `src/draft-simulator/draft_results` - data and analysis for draft simulations 
+
+### How to run:
+To run, create a `.env` folder in `src/draft-simulator` with an environment variable called `OPENAI_API_KEY` with your API key.
+Next, navigate to `src/draft-simulator`. From there, run `llm_draft_auto_drafting.py` for a single run, or `run_drafting.py` for multiple concurrent runs (can change how many you want at a time).
--- src/agents/agent_interface.py
+++ src/agents/agent_interface.py
@@ -6,23 +6,30 @@ import os
 # have a .env file in the root directory with the following content:
 # API_KEY=your_openai_api_key
 load_dotenv()
-OPENAI_API_KEY = os.getenv("API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Define config list
 config_list = [
     {
-
         # use gpt-3.5-turbo-1106 or gpt-4o-mini for POC
         # change to gpt-4 for production
         "model": "gpt-4o-mini",
         "api_key": OPENAI_API_KEY,
-        "base_url": "https://api.openai.com/v1"
+        "base_url": "https://nerc.guha-anderson.com/v1",
     }
 ]
 
+
 class BaseAgent:
-    def __init__(self, name: str, system_prompt: str, tools: List[Callable], description: str, config_list: List[dict] = config_list):
-        
+    def __init__(
+        self,
+        name: str,
+        system_prompt: str,
+        tools: List[Callable],
+        description: str,
+        config_list: List[dict] = config_list,
+    ):
+
         self.name = name
         self.system_prompt = system_prompt
         self.tools = tools
@@ -38,13 +45,11 @@ class BaseAgent:
                 "config_list": self.config_list,
                 "temperature": 0,
             },
-            description=self.description
+            description=self.description,
         )
 
         # Create a function map from the tools (functions must have __name__ defined)
-        function_map: Dict[str, Callable] = {
-            func.__name__: func for func in self.tools
-        }
+        function_map: Dict[str, Callable] = {func.__name__: func for func in self.tools}
 
         # Register the functions as tools
         agent.register_function(function_map=function_map)
